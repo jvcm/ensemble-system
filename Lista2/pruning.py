26dd1810c4018aefb31e4fac0dda08b200d78718
Joao Victor Campos Moraes <jvcm@up1c10.windows.cin.ufpe.br>
2018-09-25 17:58:05 -0300

    best first

--- Lista2/pruning.py
+++ Lista2/pruning.py
@@ -21,3 +21,24 @@ def kappa_pruning(pool = None, validation = None, best_values = 0):
             estim.append(tup[i][1])
             
     return estim
+
+def best_first_pruning(pool = None, X_val = None, y_val = None, M = 0):
+    estim = np.zeros(len(pool.estimators_))
+    for i, est in enumerate(pool.estimators_):
+        estim[i] = est.score(X_val, y_val)
+    l = np.argsort(-estim)
+    aux = pool.estimators_[:]
+    best = list()
+    best.append(pool.estimators_[l[0]])
+    l = np.delete(l, 0)
+    for i in range(1, M):
+        scores = np.zeros(len(l))
+        for k, j in enumerate(l):
+            best.append(aux[j])
+            pool.estimators_ = best
+            scores[k] = pool.score(X_val, y_val)
+            del best[i]
+        best_score_index = np.argsort(scores)[-1]
+        best.append(aux[l[best_score_index]])
+        l = np.delete(l, best_score_index)
+    return best
