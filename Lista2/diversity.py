62165588cf97d960aa062506081afe1afbbeb7ad
Joao Victor Campos Moraes <jvcm@up1c10.windows.cin.ufpe.br>
2018-09-26 22:44:49 -0300

    start analysis

---
+++ Lista2/diversity.py
@@ -0,0 +1,50 @@
+from sklearn.metrics import cohen_kappa_score
+from itertools import combinations
+import numpy as np
+
+def __coefficients(y1 = None, y2 = None, y_true = None):
+    N11 = 0
+    N10 = 0
+    N01 = 0
+    N00 = 0
+    for i in range(len(y_true)):
+        if (y1[i] == y_true[i] and y2[i] == y_true[i]):
+            N11 += 1
+        elif (y1[i] == y_true[i] and y2[i] != y_true[i]):
+            N10 += 1
+        elif(y1[i] != y_true[i] and y2[i] == y_true[i]):
+            N01 += 1
+        elif (y1[i] != y_true[i] and y2[i] != y_true[i]):
+            N00 += 1
+    coefs = np.array([N11, N10, N01, N00])
+    return coefs
+
+def kappa(pool_list = None, X_val = None, y_val = None):
+    kappa = list()
+    c = combinations(range(len(pool_list)),2)
+    tup = list()
+    for i in c:
+        tup.append(i)
+        y1 = pool_list[i[0]].predict(X_val)
+        y2 = pool_list[i[1]].predict(X_val)
+        kappa.append(cohen_kappa_score(y1, y2))
+    kappa = np.array(kappa)
+    return kappa.mean()
+
+def disagreement(pool_list = None, X_val = None, y_val = None):
+    disagree = list()
+    c = combinations(range(len(pool_list)),2)
+    tup = list()
+    Sum_Qik = 0
+    N = 0
+    for i in c:
+        disagree.append(i)
+        y1 = pool_list[i[0]].predict(X_val)
+        y2 = pool_list[i[1]].predict(X_val)
+        d = __coefficients(y1, y2, y_val)
+        Sum_Qik += (d[1] + d[2])/(d[0]+ d[1] + d[2] + d[3])
+        N += 1
+    return Sum_Qik/N
+
+
+
--- Lista2/pruning.py
+++ Lista2/pruning.py
@@ -1,34 +1,4 @@
-from sklearn.metrics import cohen_kappa_score
-from itertools import combinations
 import numpy as np
-def kappa(pool = None, X_val = None, y_val = None):
-    kappa = list()
-    c = combinations(range(len(pool.estimators_)),2)
-    tup = list()
-    for i in c:
-        tup.append(i)
-        y1 = pool.estimators_[i[0]].predict(X_val)
-        y2 = pool.estimators_[i[1]].predict(X_val)
-        kappa.append(cohen_kappa_score(y1, y2))
-    kappa = np.array(kappa)
-    l = np.argsort(-kappa)
-    estim = list()
-    aux_sc = 0
-    aux = pool.estimators_[:]
-    for i in l:
-        prune = list()
-        if tup[i][0] not in estim:
-            estim.append(tup[i][0])
-        if tup[i][1] not in estim:
-            estim.append(tup[i][1])        
-        for c in estim:
-            prune.append(aux[c])
-        pool.estimators_ = prune
-        score = pool.score(X_val, y_val)
-        if score > aux_sc:
-            aux_sc = score
-            best = prune[:]
-    return best    
 
 def reduce_error(pool = None, X_val = None, y_val = None):
     estim = np.zeros(len(pool.estimators_))
@@ -41,20 +11,40 @@ def reduce_error(pool = None, X_val = None, y_val = None):
     pool.estimators_ = best
     l = np.delete(l, 0)
     i = 1
+    score = 0
     while l != np.array([]) :
         scores = np.zeros(len(l))
-        score = pool.score(X_val, y_val)
-        print(score)
+        
         for k, j in enumerate(l):
             best.append(aux[j])
             pool.estimators_ = best
             scores[k] = pool.score(X_val, y_val)
             del best[i]
-        print(scores)
         if score < scores.max():
             best_score_index = np.argsort(scores)[-1]
             best.append(aux[l[best_score_index]])
             l = np.delete(l, best_score_index)
             i += 1
+        else:
+            return best
+        score = pool.score(X_val, y_val)
+
+def best_first(pool = None, X_val = None, y_val = None):
+    estim = np.zeros(len(pool.estimators_))
+    for i, est in enumerate(pool.estimators_):
+        estim[i] = est.score(X_val, y_val)
+    l = np.argsort(-estim)
+    aux = pool.estimators_[:]
+    best = list()
+    best.append(pool.estimators_[l[0]])
+    pool.estimators_ = best
+    l = np.delete(l, 0)
+    score = pool.score(X_val, y_val)
+    for j in l:
+        best.append(aux[j])
+        pool.estimators_ = best
+        new_score = pool.score(X_val, y_val)
+        if score < new_score:
+            score = new_score
         else:
             return best
